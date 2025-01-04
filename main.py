228b27a30e60d9953d7b64464de12e8ac9783209
Igor Maximov <imaximov@hotmail.com>
2025-01-04 07:28:17 +0000

    break after error

--- main.py
+++ main.py
@@ -24,6 +24,9 @@ def main():
                 last_announcement_cache = last_announcement
                 send_telegram_notification(last_announcement)
                 if last_announcement.find('Will List') == -1:
+                    chrome_driver.close()
+                    chrome_driver.quit()
+                    chrome_service.stop()
                     break
         sleep(sleep_period)
 
